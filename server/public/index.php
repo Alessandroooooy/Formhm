da77683dcbe697e5e7569154a70690664e574bd0
Alessandroooooy <118779774+Alessandroooooy@users.noreply.github.com>
2023-05-03 01:30:31 +0300

    Update index.php

--- server/public/index.php
+++ server/public/index.php
@@ -2,20 +2,20 @@
 
 //Быстрый звонок
 
-// Подключение к базе данных
+
 $db = new mysqli('a_level_mysql', 'myapp', 'myapp', 'iq');
 
-// Проверка соединения
+
 if ($db->connect_error) {
     die("Ошибка соединения: " . $db->connect_error);
 }
 
-// Получение данных из формы
+
 if (isset($_POST['phone'])) {
     $phone = trim($_POST['phone']);
     
-    // Сохранение данных в базе данных
-    $query = "INSERT INTO calls (phone, created_at, updated_at, process) VALUES ('$phone', NOW(), NOW(), false)";
+    
+   $query = "INSERT INTO calls (phone, created_at, updated_at, process) VALUES ('$phone', NOW(), NOW(), false)";
     $result = $db->query($query);
     
     if ($result) {
